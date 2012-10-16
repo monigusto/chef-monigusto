a256a67dd2fb300995630f534d3c011b3279dfce
Ulf Mansson <ulf.mansson@recordedfuture.com>
2012-10-16 12:49:23 +0200

    Added Sensu, Redis and RabbitMQ to the server, will start all processes needed for Sensu and will also install the sensu-community-plugin package

--- recipes/server.rb
+++ recipes/server.rb
@@ -26,5 +26,13 @@ include_recipe "collectd::server"
 include_recipe "collectd::collectd_web"
 include_recipe 'collectd-elasticsearch'
 include_recipe 'ntp'
-
+include_recipe 'rabbitmq'
+include_recipe 'rabbitmq::mgmt_console'
+include_recipe 'redis'
+include_recipe 'sensu::rabbitmq'
+include_recipe 'sensu::client'
+include_recipe 'sensu::api'
+include_recipe 'sensu::dashboard'
+include_recipe 'sensu::server'
+include_recipe 'sensu-community-plugins'
 include_recipe "monigusto::server_collectd"
